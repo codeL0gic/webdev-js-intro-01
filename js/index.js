26132f8deff6377fe2df47eb4d7d3aed46994e2c
codeL0gic <shannon.neumann@yum.com>
2025-01-12 11:57:35 -0500

    main

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const six = 6;
+console.log(six);
+
+const fifteen = 15;
+console.log(fifteen);
+
+let bestson = "jordan";
+console.log(bestson);
+*/
+
